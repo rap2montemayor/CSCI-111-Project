29cc50cbd1bab1cd39d1440dd4653e00e516b34b
Raprap Montemayor <rap2montemayor@gmail.com>
2021-11-05 06:19:32 +0800

    Added an enum for the state of each tile, and modified nextIteration to return indices of modified tiles.

--- ai/AI.hpp
+++ ai/AI.hpp
@@ -4,9 +4,17 @@
 // To do
 
 class AI {
-    std::vector<std::vector<int>>& board; 
+    enum TileState {
+        Passable = 0b000001,
+        Dark     = 0b000010,
+        HasGhost = 0b000100,
+        HasChild = 0b001000,
+        ToVisit  = 0b010000,
+        Visited  = 0b100000
+    };
+    std::vector<std::vector<int>>& board;
 
 public:
     virtual AI(std::vector<std::vector<int>>& board) = 0;    
-    virtual void nextIteration() = 0;
+    virtual vector<pair<int, int>> nextIteration() = 0;
 };
